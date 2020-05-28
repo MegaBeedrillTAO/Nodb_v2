a4ec970b49daf8df1cff08b270a08f59534ca2a8
MegaBeedrillTAO <jakekremling@yahoo.com>
2020-05-28 10:58:06 -0500

    Worked more on backend character maker

---
+++ server/CharacterFiles/races.js
@@ -0,0 +1,15 @@
+const races = [
+    {
+        name: "Human",
+        subrace: [
+            {
+                subName: "",
+                stats: "+1 to all stats"
+            }
+        ]
+    }
+]
+
+module.exports = {
+    races
+}
--- server/Controllers/characterController.js
+++ server/Controllers/characterController.js
@@ -1,24 +1,94 @@
 const classes = require('../CharacterFiles/classes')
+const races = require('../CharacterFiles/races')
+var sentRace = ''
+var sentSubRace = ''
+var sentClass = ''
+var sentSubClass = ''
+var sentGender = ''
+var sentBackground =''
+var sentStatMods = ''
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
 
-
-async function makeCharacter(req, res){
-    var sentRace = ''
-    var sentSubRace = ''
-    var sentClass = ''
-    var sentSubClass = ''
-    var sentGender = ''
-    const {race, charClass, gender} = req.body
+function getClass(charClass){
     if (charClass == ''){
         let classNum = getRandomInt(classes.classes.length)
         let subClassArray = [...classes.classes[classNum]["subclass"]]
-        let subClassNum = getRandomInt(subClassArray.length + 1)
+        let subClassNum = getRandomInt(subClassArray.length)
         sentClass = classes.classes[classNum].name
         sentSubClass = subClassArray[subClassNum]
     }
+    else {
+        sentClass = charClass
+        let subClassArray =[]
+        for (let i = 0; i < classes.classes.length; i++){
+            if (classes.classes[i].name == sentClass){
+                subClassArray = [...classes.classes[i].subclass]
+            }
+        }
+        let subClassNum = getRandomInt(subClassArray.length)
+        sentSubClass = subClassArray[subClassNum]
+    }
+}
+
+function getRace(race){
+    if (race == ''){
+        let raceNum = getRandomInt(races.races.length)
+        let subRaceArray = [...races.races[raceNum].subrace]
+        let subRaceNum = getRandomInt(subRaceArray.length)
+        sentRace = races.races[raceNum].name
+        sentSubRace = subRaceArray[subRaceNum].subName
+        sentStatMods = subRaceArray[subRaceNum].stats
+    }
+    else {
+        sentRace = race
+        let subRaceArray = []
+        for (let i = 0; i < races.races.length; i++){
+            if (races.races[i].name == sentRace){
+                subRaceArray = [...races.races[i].subrace]
+            }
+        }
+        let subRaceNum = getRandomInt(subRaceArray.length)
+        sentSubRace = subRaceArray[subRaceNum].subName
+        sentStatMods = subRaceArray[subRaceNum].stats
+    }
+}
+
+function getGender(gender){
+    let genderNum = 0
+    if (gender == ''){
+         genderNum = getRandomInt(2)
+
+    }
+    if (genderNum == 0){
+        sentGender = 'Male'
+    }
+    else if (genderNum == 1){
+        sentGender = "Female"
+    }
+    else {
+        sentGender = "A mistake was made"
+    }
+}
+
+async function makeCharacter(req, res){
+    
+    const {race, charClass, background, gender} = req.body
+    
+    getClass(charClass)
+    getRace(race)
+    getGender(gender)
+    
+    res.status(200).json({
+        sentClass,
+        sentSubClass,
+        sentRace,
+        sentSubRace,
+        sentStatMods,
+        sentGender
+    })
 }
 
 module.exports = {
