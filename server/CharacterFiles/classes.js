19e8bb6dc3af1be2a2b1dd778857a5d356d2318e
MegaBeedrillTAO <jakekremling@yahoo.com>
2020-06-02 12:36:13 -0500

    Added more classes and races to static files

--- server/CharacterFiles/classes.js
+++ server/CharacterFiles/classes.js
@@ -16,6 +16,133 @@ const classes = [
             "Artillerist",
             "Battle Smith"
         ]
+    },
+    {
+        name: "Bard",
+        subclass: [
+            "College of Lore",
+            "College of Valor",
+            "College of Glamour",
+            "College of Blades",
+            "College of Whispers"
+        ]
+    },
+    {
+        name: "Cleric",
+        subclass: [
+            "Grave Domain",
+            "Order Domain",
+            "Knowledge Domain",
+            "Life Domain",
+            "Light Domain",
+            "Nature Domain",
+            "Tempest Domain",
+            "Trickery Domain",
+            "War Domain",
+            "Forge Domain"
+        ]
+    },
+    {
+        name: "Druid",
+        subclass: [
+            "Circle of Spores",
+            "Circle of the Moon",
+            "Circle of the Land",
+            "Circle of Dreams",
+            "Circle of the Shepard"
+        ]
+    },
+    {
+        name: "Fighter",
+        subclass: [
+            "Echo Knight",
+            "Champion",
+            "Battle Master",
+            "Eldritch Knight",
+            "Arcane Archer",
+            "Samurai",
+            "Cavalier"
+        ]
+    },
+    {
+        name: "Monk",
+        subclass: [
+            "Way of the Open Hand",
+            "Way of Shadow",
+            "Way of the Four Elements",
+            "Way of the Drunken Master",
+            "Way of the Kensei",
+            "Way of the Sun Soul"
+        ]
+    },
+    {
+        name: "Paladin",
+        subclass: [
+            "Oath of Devotion",
+            "Oath of Vengeance",
+            "Oath of the Ancients",
+            "Oath of Conquest",
+            "Oath of Redemption",
+            "Oathbreaker"
+        ]
+    },
+    {
+        name: "Ranger",
+        subclass: [
+            "Hunter",
+            "Beast Master",
+            "Gloom Stalker",
+            "Horizon Walker",
+            "Monster Slayer"
+        ]
+    },
+    {
+        name: "Rogue",
+        subclass: [
+            "Thief",
+            "Assassin",
+            "Arcane Trickster",
+            "Inquisitive",
+            "Mastermind",
+            "Scout",
+            "Swashbuckler"
+        ]
+    },
+    {
+        name: "Sorcerer",
+        subclass: [
+            "Wild Magic",
+            "Draconic Bloodline",
+            "Divine Soul",
+            "Shadow Magic",
+            "Storm Sorcery"
+        ]
+    },
+    {
+        name: "Warlock",
+        subclass: [
+            "The Archfey",
+            "The Fiend",
+            "The Great Old One",
+            "The Celestial",
+            "The Hexblade"
+        ]
+    },
+    {
+        name: "Wizard",
+        subclass: [
+            "School of Abjuration",
+            "School of Conjuration",
+            "School of Divination",
+            "School of Enchantment",
+            "School of Evocation",
+            "School of Illusion",
+            "School of Necromancy",
+            "School of Transmutation",
+            "War Magic",
+            "Chronurgy Magic",
+            "Graviturgy Magic"
+        ]
     }
 ]
 
--- server/CharacterFiles/races.js
+++ server/CharacterFiles/races.js
@@ -7,6 +7,23 @@ const races = [
                 stats: "+1 to all stats"
             }
         ]
+    },
+    {
+        name: "Dwarf",
+        subrace: [
+            {
+                subName: "Hill",
+                stats: "+2 to Con, +1 to Wis"
+            },
+            {
+                subName: "Mountain",
+                stats: "+2 to Con, +2 to Str"
+            },
+            {
+                subName: "Duergar",
+                stats: "+2 to Con, +1 to Str"
+            }
+        ]
     }
 ]
 
