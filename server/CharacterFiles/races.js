60cfd206d6dc8b17a2c3aa842fa6a3d4f3181e68
MegaBeedrillTAO <jakekremling@yahoo.com>
2020-06-09 12:56:05 -0500

    Added more races to the backend file

--- server/CharacterFiles/races.js
+++ server/CharacterFiles/races.js
@@ -24,6 +24,99 @@ const races = [
                 stats: "+2 to Con, +1 to Str"
             }
         ]
+    },
+    {
+        name: "Elf",
+        subrace: [
+            {
+                subName: "High Elf",
+                stats: "+2 to Dex, +1 to Int"
+            },
+            {
+                subName: "Wood Elf",
+                stats: "+2 to Dex, +1 to Wis"
+            },
+            {
+                subName: "Drow",
+                stats: "+2 to Dex, +1 to Cha"
+            },
+            {
+                subName: "Eladrin",
+                stats: "+2 to Dex, +1 to Cha"
+            },
+            {
+                subName: "Sea Elf",
+                stats: "+2 to Dex, +1 to Con"
+            },
+            {
+                subName: "Shadar-Kai",
+                stats: "+2 to Dex, +1 to Con"
+            },
+            {
+                subName: "Pallid Elf",
+                stats: "+2 to Dex, +1 to Con"
+            }
+        ]
+    },
+    {
+        name: "Halfling",
+        subrace: [
+            {
+                subName: "Lightfoot",
+                stats: "+2 to Dex, +1 to Cha"
+            },
+            {
+                subName: "Stout",
+                stats: "+2 to Dex, +1 to Con"
+            },
+            {
+                subName: "Lotusden",
+                stats: "+2 to Dex, +1 to Wis"
+            }
+        ]
+    },
+    {
+        name: "Dragonborn",
+        subrace: [
+            {
+                subName: "",
+                stats: "+2 to Str, +1 to Cha"
+            },
+            {
+                subName: "Draconblood",
+                stats: "+2 to Int, +1 to Cha"
+            },
+            {
+                subName: "Ravenite",
+                stats: "+2 to Str, +1 to Con"
+            }
+        ]
+    },
+    {
+        name: "Gnome",
+        subrace: [
+            {
+                subName: "Forrest Gnome",
+                stats: "+2 to Int, +1 to Dex"
+            },
+            {
+                subName: "Rock Gnome",
+                stats: "+2 to Int, +1 to Con"
+            },
+            {
+                subName: "Deep Gnome",
+                stats: "+2 to Int, +1 to Dex"
+            }
+        ]
+    },
+    {
+        name: "Half-Elf",
+        subrace: [
+            {
+                subName: "",
+                stats: "+2 to Cha, +1 to any other two stats"
+            }
+        ]
     }
 ]
 
